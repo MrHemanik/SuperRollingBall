874216a896f46210de3de7e87f000824b57db325
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2021-04-27 16:12:35 +0200

    Added WaitTime on Start- and EndPoint in MovingPlattformScript.cs

--- Projekt/Assets/Scripts/MovingPlattformScript.cs
+++ Projekt/Assets/Scripts/MovingPlattformScript.cs
@@ -4,37 +4,49 @@ using UnityEngine;
 
 public class MovingPlattformScript : MonoBehaviour
 {
-    private Vector3 startPoint;
-    private Vector3 endPoint;
+    private Vector3 _startPoint;
+    private Vector3 _endPoint;
     public Vector3 moveDirection;
     public float movingTime = 4.0f;
-    public float waitOnPoint = 1.0f; //Zeit, die er am Punkt wartet.
+    public float pauseTime = 1.0f; //Zeit, die er am Punkt wartet.
+    public bool pauseInMid = true;
+    private float _waitTilNextCheckTime;
+    private float _timer = 0.0f;
 
     void Start()
     {
-        startPoint = transform.position;
-        endPoint = startPoint + moveDirection;
+        _startPoint = transform.position;
+        _endPoint = _startPoint + moveDirection;
+        _waitTilNextCheckTime = movingTime / 2 + pauseTime;
     }
 
     void Update()
     {
-        Vector3 currentPosition = transform.position;
-        //Ich habe dafür über 4 Stunden meines Lebens verschwendet :) Hab vieles Verkompliziert.
-        //Wenn eine Koordinate über oder unter Start und Endpunkt ist, muss Richtung gewechselt werden.
-        if (currentPosition.x > Mathf.Max(startPoint.x,endPoint.x) || 
-            currentPosition.x < Mathf.Min(startPoint.x,endPoint.x) || //MathMinMax geht hier auch
-            currentPosition.y > startPoint.y && currentPosition.y > endPoint.y || 
-            currentPosition.y < startPoint.y && currentPosition.y < endPoint.y ||
-            currentPosition.z > startPoint.z && currentPosition.z > endPoint.z || 
-            currentPosition.z < startPoint.z && currentPosition.z < endPoint.z)
+        if (_timer > pauseTime) //Sobald der Timer die wartezeit bis zum nächsten Check überläuft, dann darf wieder gechecked werden
         {
-            //Debug.Log("Umdrehen" + moveDirection + startPoint + endPoint);
-            //Kann zu Problemen führen, falls es zu größeren Lags kommt
-            moveDirection = moveDirection * -1;
-            //TODO: Timer aktivieren, aber der hält nicht in Mitte, was nervig ist.
+            Vector3 currentPosition = transform.position;
+            if(_timer > _waitTilNextCheckTime){ //Erst wenn der Timer die Wartezeit bis zum Check durchlaufen hat, darf er wieder checken
+                //Ich habe dafür über 4 Stunden meines Lebens verschwendet :) Hab vieles Verkompliziert.
+                //Wenn eine Koordinate über oder unter Start und Endpunkt ist, muss Richtung gewechselt werden.
+                if (currentPosition.x > Mathf.Max(_startPoint.x, _endPoint.x) ||
+                    currentPosition.x < Mathf.Min(_startPoint.x, _endPoint.x) || //MathMinMax geht hier auch
+                    currentPosition.y > _startPoint.y && currentPosition.y > _endPoint.y ||
+                    currentPosition.y < _startPoint.y && currentPosition.y < _endPoint.y ||
+                    currentPosition.z > _startPoint.z && currentPosition.z > _endPoint.z ||
+                    currentPosition.z < _startPoint.z && currentPosition.z < _endPoint.z)
+                {
+                    //Debug.Log("Umdrehen" + moveDirection + startPoint + endPoint);
+                    //Kann zu Problemen führen, falls es zu größeren Lags kommt
+                    moveDirection = moveDirection * -1; //Umdrehen
+                    _timer = 0.0f;
+                }
+            }
+            transform.position = currentPosition + (moveDirection / movingTime * Time.deltaTime); //Bewegen
         }
-        transform.position = currentPosition+(moveDirection/movingTime*Time.deltaTime);
+        _timer += Time.deltaTime;
+
         
-        
+
+
     }
 }
