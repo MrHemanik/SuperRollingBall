1899639608946c8b4e94c04e8b2db6bd14146285
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2021-04-27 20:28:47 +0200

    Added MovingPlatformScript waitStartEnd and waitMid Time. Changed Platforms in DemoLevel-Scene accordingly

--- Projekt/Assets/Scripts/MovingPlattformScript.cs
+++ Projekt/Assets/Scripts/MovingPlattformScript.cs
@@ -1,51 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingPlattformScript : MonoBehaviour
 {
-    private Vector3 _startPoint;
-    private Vector3 _endPoint;
     public Vector3 moveDirection;
-    public float movingTime = 4.0f; //Zeit, die bis zum Punkt gebraucht wird (exklusive pauseTime)
-    public float pauseTime = 1.0f; //Zeit, die er am Punkt wartet.
-    private float _waitTilNextCheckTime; //Zum Schutz gegen Festhängen durch Lags
-    private float _timer = 0.0f;
+    public float movingTime = 4.0f;
+    public float pauseTimeStartEnd = 1.0f; //Zeit, die er am Punkt wartet.
+    public float pauseTimeMid = 1.0f; //Zeit, die er am Punkt wartet.
+    private Vector3 _endPoint;
+    private Vector3 _startPoint;
 
-    void Start()
+    private void Start()
     {
         _startPoint = transform.position;
         _endPoint = _startPoint + moveDirection;
-        _waitTilNextCheckTime = movingTime/2 + pauseTime;
+        Debug.Log(_startPoint +""+ _endPoint);
     }
 
-    void Update()
+    private void Update()
     {
-        if (_timer > pauseTime) //Sobald der Timer die wartezeit bis zum nächsten Check überläuft, dann darf wieder gechecked werden
-        {
-            Vector3 currentPosition = transform.position;
-            if(_timer > _waitTilNextCheckTime){ //Erst wenn der Timer die Wartezeit bis zum Check durchlaufen hat, darf er wieder checken
-                //Ich habe dafür über 4 Stunden meines Lebens verschwendet :) Hab vieles Verkompliziert.
-                //Wenn eine Koordinate über oder unter Start und Endpunkt ist, muss Richtung gewechselt werden.
-                if (currentPosition.x > Mathf.Max(_startPoint.x, _endPoint.x) ||
-                    currentPosition.x < Mathf.Min(_startPoint.x, _endPoint.x) || //MathMinMax geht hier auch
-                    currentPosition.y > _startPoint.y && currentPosition.y > _endPoint.y ||
-                    currentPosition.y < _startPoint.y && currentPosition.y < _endPoint.y ||
-                    currentPosition.z > _startPoint.z && currentPosition.z > _endPoint.z ||
-                    currentPosition.z < _startPoint.z && currentPosition.z < _endPoint.z)
-                {
-                    //Debug.Log("Umdrehen" + moveDirection + startPoint + endPoint);
-                    //Kann zu Problemen führen, falls es zu größeren Lags kommt
-                    moveDirection = moveDirection * -1; //Umdrehen
-                    _timer = 0.0f;
-                }
-            }
-            transform.position = currentPosition + (moveDirection / movingTime * Time.deltaTime); //Bewegen
-        }
-        _timer += Time.deltaTime;
-
-        
-
-
+        transform.position = GetPosition(Time.time);
+    }
+    
+    //Das hat ja mal so viel Zeit gekostet, das so umzusetzen.
+    private Vector3 GetPosition(float time)
+    {
+        var intervalTime = 2 * (movingTime + pauseTimeStartEnd + pauseTimeMid); //Intervalllänge
+        var timer = time % intervalTime; //Timer von 0 bis Intervalllänge
+        Vector3 position;
+        if (timer < movingTime / 2) //Hinweg erste Hälfte
+            position = _startPoint + moveDirection / movingTime * timer;
+        else if (timer < movingTime / 2 + pauseTimeMid) //Pause Hinweg
+            position = _startPoint + moveDirection / 2;
+        else if (timer < movingTime + pauseTimeMid) //Hin zweite Hälfte
+            position = _startPoint + moveDirection / movingTime * (timer - pauseTimeMid);
+        else if (timer < movingTime + pauseTimeStartEnd + pauseTimeMid) //Pause Ende
+            position = _endPoint;
+        else //Die zweite Hälfte ist die an _endPoint gespiegelte erste Hälfte: f(_intervalTime) = _endPoint +_startPoint - f(0)
+            position = _endPoint + _startPoint - GetPosition(timer - intervalTime / 2);
+        return position;
     }
 }
